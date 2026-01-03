bf00c8a3adc68ac2cd8b987832ffcd2cb8ebfa28
Mohak Sharma <mohaksharma1412@gmail.com>
2026-01-03 15:37:39 +0530

    fix env imports

--- frontend/src/services/contactService.js
+++ frontend/src/services/contactService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class ContactService {
   constructor() {
     this.api = axios.create({
-      baseURL: 'http://localhost:5000/api',
+      baseURL: import.meta.env.VITE_SERVER_URI || 'http://localhost:5000/api',
     });
   }
 
