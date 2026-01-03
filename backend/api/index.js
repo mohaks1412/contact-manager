9013d008d886cf988bdfef3bf5f5765eb99eccc9
Mohak Sharma <mohaksharma1412@gmail.com>
2026-01-03 16:38:21 +0530

    fix index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,23 @@
-import { handler as createHandler } from 'serverless-http';
-import app from '../server.js';
-export const handler = createHandler(app);
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import contactsRouter from './routes/contact.js';
+
+dotenv.config();
+const app = express();
+
+app.use(cors({
+  origin: process.env.FRONTEND_URI,  // Frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.use('/contacts', contactsRouter);
+
+export default app;
