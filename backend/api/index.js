31b41229e60e6879f9696beea7c9da7d7eb189b7
Mohak Sharma <mohaksharma1412@gmail.com>
2026-01-03 17:29:26 +0530

    hc

--- backend/api/index.js
+++ backend/api/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "https://contact-manager-4vxi.vercel.app/",
+  origin: "https://contact-manager-4vxi.vercel.app",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
